eef3c030e54d0fdbcf88b8ae897670adaff7b41f
sameedakber-ai <sameedakber.ai@gmail.com>
2025-01-16 22:40:28 +0500

    Add endpoints to create and authenticate user

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
 
-# Create your views here.
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework import status
+
+from .serializers import UserSerializer
+
+
+class UserCreateView(APIView):
+    permission_classes = [AllowAny]
+
+    def post(self, request, *args, **kwargs):
+        serializer = UserSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        user = serializer.save()
+        serializer = UserSerializer(user)
+
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
