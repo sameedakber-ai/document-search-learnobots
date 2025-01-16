6e0701f0dc817a2722892255d76f4cfbbd625565
sameedakber-ai <sameedakber.ai@gmail.com>
2025-01-16 22:15:30 +0500

    Add default authentication and permission classes

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -31,6 +31,7 @@ SECRET_KEY = 'django-insecure-2ye(!87k##v=8(a39u-h-1oz8ps(%4tvhr!zxc8uho5#aon+--
 DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
+
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
@@ -54,6 +55,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'api',
+    'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -64,6 +68,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'backend.urls'
@@ -138,3 +143,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOWED_ALLOWED_ORIGINS = True
+CORS_ALLOWED_CREDENTIALS = True
