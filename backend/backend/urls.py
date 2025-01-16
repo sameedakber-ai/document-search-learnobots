4e3ea7e1d08e2964c7feea75e3b8fb413046f309
sameedakber-ai <sameedakber.ai@gmail.com>
2025-01-16 22:50:05 +0500

    Add api urls

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
-from ..api.views import UserCreateView
+from api.views import UserCreateView
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
