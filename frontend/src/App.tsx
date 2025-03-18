12be7a3e6c72a0ba452d57ef7a8753a96800a0f3
Sameed Akber <sameedakber.ai@gmail.com>
2025-03-18 15:07:53 +0500

    Add workflow component to react app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,42 +1,50 @@
-import react from "react"
-import {BrowserRouter, Routes, Route, Navigation, Navigate} from "react-router-dom";
+import {BrowserRouter, Routes, Route, Navigate} from "react-router-dom";
 import Login from "./pages/Login"
 import Register from "./pages/Register";
 import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
 import ProtectedRoute from "./components/ProtectedRoute.tsx";
+import Workflow from "./components/Workflow.tsx";
 
-function Logout () {
-  localStorage.clear()
-  return <Navigate to="/login" />
+function Logout() {
+    localStorage.clear()
+    return <Navigate to="/login"/>
 }
 
 function RegisterAndLogout() {
-  localStorage.clear()
-  return <Register />
+    localStorage.clear()
+    return <Register/>
 }
 
 function App() {
 
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route
-            path="/"
-            element={
-              <ProtectedRoute>
-                <Home />
-              </ProtectedRoute>
-            }
-        />
-        <Route path="/login" element={<Login />}/>
-          <Route path="/logout" element={<Logout />}/>
-        <Route path="/register" element={<RegisterAndLogout />}/>
-        <Route path="*" element={<NotFound />}/>
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route
+                    path="/"
+                    element={
+                        <ProtectedRoute>
+                            <Home/>
+                        </ProtectedRoute>
+                    }
+                />
+                <Route
+                    path="/workflows/:id"
+                    element={
+                        <ProtectedRoute>
+                            <Workflow />
+                        </ProtectedRoute>
+                    }
+                />
+                <Route path="/login" element={<Login/>}/>
+                <Route path="/logout" element={<Logout/>}/>
+                <Route path="/register" element={<RegisterAndLogout/>}/>
+                <Route path="*" element={<NotFound/>}/>
 
-      </Routes>
-    </BrowserRouter>
-  )
+            </Routes>
+        </BrowserRouter>
+    )
 }
 
 export default App
